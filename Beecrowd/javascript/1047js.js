e5b9a0c09dd27b7c4db2c1d35ab5f97f630bfa26
Anthony|Fi&Math <73728737+pereira3R@users.noreply.github.com>
2023-07-02 13:03:44 -0400

    Update 1047js.js
    Signed-off-by: Anthony|Fi&Math <73728737+pereira3R@users.noreply.github.com>

--- Beecrowd/javascript/1047js.js
+++ Beecrowd/javascript/1047js.js
@@ -1,15 +1,15 @@
-var input = require('fs').readFileSync('/dev/sdtin', 'utf8');
-var [a,b,c,d] = input.split('\n').map(Number);
+const [horaInicial, minutoInicial, horaFinal, minutoFinal] = require('fs').readFileSync('/dev/stdin', 'utf8').split(' ').map(Number);
 
-var diferenceH = a - c;
-var diferenceM = 0;
-if(a == b == c == d){
-    console.log("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
-}if(b > d){
-    diferenceH -= 1; 
-    diferenceM = 60 - (b - d); 
-    console.log("O JOGO DUROU " + diferenceH + " HORA(S) E " + diferenceM + " MINUTO(S)");
-}else if(b < d){
-    diferenceM = (d - b); 
-    console.log("O JOGO DUROU " + diferenceH + " HORA(S) E " + diferenceM + " MINUTO(S)");
+const minutosInicial = horaInicial * 60 + minutoInicial;
+const minutosFinal = horaFinal * 60 + minutoFinal;
+
+let duracaoMinutos = (minutosFinal >= minutosInicial) ? minutosFinal - minutosInicial : 24 * 60 - (minutosInicial - minutosFinal);
+
+const duracaoHoras = Math.floor(duracaoMinutos / 60);
+const duracaoMinutosRestantes = duracaoMinutos % 60;
+
+if (horaFinal === minutoInicial && minutoInicial === horaFinal && horaFinal === minutoFinal) {
+    console.log(`O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)`);
+}else{
+    console.log(`O JOGO DUROU ${duracaoHoras} HORA(S) E ${duracaoMinutosRestantes} MINUTO(S)`);
 }
