70821d2a02ede2103c0b04571716b8b7f02fbd61
Anthony|Fi&Math <73728737+pereira3R@users.noreply.github.com>
2023-08-24 20:20:45 -0400

    Delete scriptCounter.py
    Signed-off-by: Anthony|Fi&Math <73728737+pereira3R@users.noreply.github.com>

--- scriptCounter.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def contar_arquivos(diretorio):
-    return len([arquivo for arquivo in os.listdir(diretorio) if os.path.isfile(os.path.join(diretorio, arquivo))])
-
-diretorio_alvo = "/caminho/do/seu/diretorio"
-numero_de_arquivos = contar_arquivos(diretorio_alvo)
-
-print(f"O número de arquivos em {diretorio_alvo} é: {numero_de_arquivos}")
